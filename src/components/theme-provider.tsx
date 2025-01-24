130b0e03526f5f7776d78fa72b6e504fac877c6e
sanjbyte <sana.devil16@gmail.com>
2025-01-23 18:03:57 -0800

    Fix all these errors

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,10 +1,15 @@
 "use client"
 
-import * as React from "react"
+import type * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: {
+  children: React.ReactNode
+  [key: string]: any
+}) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
