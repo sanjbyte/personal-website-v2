9c50d8a1dd24cac2941568b0f872d63b017e7b6e
sanjbyte <sana.devil16@gmail.com>
2025-01-24 17:41:38 -0800

    Update next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      }
+    }
+    // Print out webpack config for debugging
+    console.log("Webpack config:", JSON.stringify(config, null, 2))
+    return config
+  },
 }
 
 module.exports = nextConfig
